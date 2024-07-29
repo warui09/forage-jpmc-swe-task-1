aaba5886d88a5f074f28709ed300173ccabd8022
Kevin Warui <waruimacharia@gmail.com>
2024-07-29 11:58:50 +0300

    Update client_test.py

--- client_test.py
+++ client_test.py
@@ -25,8 +25,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-      stock_a, bid_price_a, ask_price_a, price_a = getDataPoint(quotes[0])
-      stock_b, bid_price_b, ask_price_b, price_b = getDataPoint(quotes[1])
+      _, _, _, price_a = getDataPoint(quotes[0])
+      _, _, _, price_b = getDataPoint(quotes[1])
       self.assertEqual(getRatio(price_a, price_b), price_a / price_b)
 
 
